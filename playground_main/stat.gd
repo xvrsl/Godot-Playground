2466ed3d42f09be2832b8e6eed092a6aeddc9776
Yueyang Wu <xavierseal@live.com>
2025-02-26 14:05:02 +0800

    update

--- playground_main/stat.gd
+++ playground_main/stat.gd
@@ -1,24 +1,86 @@
-class Stat:
-	var baseValue:float
-	var modifiers:Array[StatModifier]
-	var Value:
-		get:
-			if _dirty:
-				CalculateValue()
-			return _cachedValue
-	var _cachedValue:float
-	
-	var _dirty:bool
-	func CalculateValue():
-		_dirty = false
-		
-class StatModifier:
-	var value:float
-	var modifierType:StatModifierType
+
+
+class_name Stat
 
 enum StatModifierType
 {
-	Add,
-	PercentAdd,
-	Multiply
+	Add = 0,
+	PercentAdd= 1000,
+	Multiply = 2000
 }
+
+var baseValue:float
+var _modifiers:Array[StatModifier]
+var Value:
+	get:
+		if Dirty:
+			_calculate_value()
+		return _cachedValue
+var _cachedValue:float
+
+var _needResort:bool
+var _dirty:bool
+var Dirty:
+	get:
+		if _dirty: 
+			return true
+		if _needResort:
+			return true
+		return false
+
+func _calculate_value():
+	if _needResort:
+		_resort()
+	var result := baseValue
+	var percentAdding := false
+	var percentAddPriority := 0
+	var percentAddBuffer := 0.0
+	var applyPercentAddingFunc = func ():
+		result *= 1+percentAddBuffer
+		percentAdding = false
+		percentAddBuffer = 0
+		percentAddPriority = 0
+		
+	for modifier in _modifiers:
+		if percentAdding && (
+			modifier.type != StatModifierType.PercentAdd 
+			|| percentAddPriority != modifier.Priority
+		): #apply buffer
+			applyPercentAddingFunc.call()
+		
+		match modifier.type:
+			StatModifierType.Add:
+				result += modifier.value
+			StatModifierType.PercentAdd:
+				percentAdding = true
+				percentAddPriority = modifier.Priority
+				percentAddBuffer += modifier.value
+			StatModifierType.Multiply:
+				result *= modifier.value
+	if percentAdding:
+		applyPercentAddingFunc.call()
+		
+	_dirty = false
+	
+	
+func _resort():
+	_modifiers.sort_custom(StatModifier.compare)
+	_needResort = false
+	
+func add_modifier(modifier:StatModifier):
+	_modifiers.append(modifier)
+	_needResort = true
+	_dirty = true
+		
+class StatModifier:
+	var value:float
+	var type:StatModifierType
+	var overridePriority :bool
+	var overridePriorityValue := 0
+	var Priority:
+		get:
+			if overridePriority:
+				return overridePriorityValue
+			return int(type)
+	static func compare(a:StatModifier,b:StatModifier):
+			return a.Priority < b.Priority
--- playground_main/stat_holder.gd
+++ playground_main/stat_holder.gd
@@ -1,1 +1,21 @@
 extends Node
+
+@export var valueDisplay := 0.0
+
+var stat:Stat
+
+func _ready():
+	stat = Stat.new()
+	stat.baseValue 
+
+func _process(delta:float)->void:
+	valueDisplay = stat.Value
+	if Input.is_action_just_pressed("ui_accept"):
+		var mod = Stat.StatModifier.new()
+		mod.value = 1
+		stat.add_modifier(mod)
+	if Input.is_action_just_pressed("ui_cancel"):
+		var mod = Stat.StatModifier.new()
+		mod.type = Stat.StatModifierType.PercentAdd
+		mod.value = 0.1
+		stat.add_modifier(mod)
